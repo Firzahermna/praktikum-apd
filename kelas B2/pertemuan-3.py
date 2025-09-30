7d165f877e7ad3b8507ea98506cd3ba501d5cb40
Firzahermna <firzahermana467@gmail.com>
2025-09-30 22:01:25 +0800

    posttest3

---
+++ kelas B2/pertemuan-3.py
@@ -0,0 +1,14 @@
+nilai = 70
+if nilai >= 60:
+    status = "Lulus"
+else:
+    status = "Tidak Lulus"
+print(status)
+# output
+Lulus
+# Menggunakan Ternary Operator
+nilai = 70
+status = "Lulus" if nilai >= 60 else "Tidak Lulus"
+print(status)
+# output
+Lulus
